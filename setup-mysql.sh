101338dcf8e5488ef483287084c820089ccba2bf
Tho Trinh <trphuctho@gmail.com>
2021-04-21 13:49:01 +0000

    bind to host network

--- setup-mysql.sh
+++ setup-mysql.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-HOST_BIND_ADDRESS=127.0.0.1
 MYSQL_ROOT_PASSWORD=root
 
 MYSQL_USER=mysql
@@ -43,7 +42,7 @@ MYSQL_USER_ID=$(echo "${MYSQL_USER}" | xargs id -u)
 MYSQL_GROUP_ID=$(echo "${MYSQL_GROUP}" | xargs getent group | cut -d: -f3)
 
 docker run -d --restart=unless-stopped --name=mysql \
-	-p ${HOST_BIND_ADDRESS}:3306:3306 \
+	--network=host
 	-v ${MY_CNF}:/etc/my.cnf:ro \
 	-v ${DATA_DIR}:/var/lib/mysql \
 	-v ${LOG_DIR}:/var/log/mysql \
--- start-mysql.sh
+++ start-mysql.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-HOST_BIND_ADDRESS=127.0.0.1
-
 MYSQL_USER=mysql
 MYSQL_GROUP=mysql
 
@@ -14,8 +12,8 @@ MY_CNF="${CONFIG_DIR}/my.cnf"
 MYSQL_USER_ID=$(echo "${MYSQL_USER}" | xargs id -u)
 MYSQL_GROUP_ID=$(echo "${MYSQL_GROUP}" | xargs getent group | cut -d: -f3)
 
-docker run -d --restart=unless-stopped --name=mysql \
-	-p ${HOST_BIND_ADDRESS}:3306:3306 \
+docker run -d --restart=unless-stopped --name=mysql-server \
+	--network=host \
 	-v ${MY_CNF}:/etc/my.cnf:ro \
 	-v ${DATA_DIR}:/var/lib/mysql \
 	-v ${LOG_DIR}:/var/log/mysql \
