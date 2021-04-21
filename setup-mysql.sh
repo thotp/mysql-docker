bd11438b87a3877498f6fbbd95501f02b1a41e9d
Tho Trinh <trphuctho@gmail.com>
2021-04-21 13:57:38 +0000

    add group replication & cluster configs

--- setup-mysql.sh
+++ setup-mysql.sh
@@ -30,12 +30,43 @@ datadir=/var/lib/mysql
 character-set-server=utf8mb4
 collation-server=utf8mb4_unicode_ci
 
-log-bin=/var/log/mysql/bin.log
+#log-bin=/var/log/mysql/bin.log
 log-error=/var/log/mysql/error.log
 general-log=1
 general-log-file=/var/log/mysql/general.log
 slow-query-log=1
 slow-query-log-file=/var/log/mysql/slowquery.log
+
+# Group Replication
+# https://dev.mysql.com/doc/refman/8.0/en/group-replication-configuring-instances.html
+#
+disabled_storage_engines="MyISAM,BLACKHOLE,FEDERATED,ARCHIVE,MEMORY"
+
+server_id=1
+gtid_mode=ON
+enforce_gtid_consistency=ON
+
+log_bin=binlog
+log_slave_updates=ON
+binlog_format=ROW
+#master_info_repository=TABLE (deprecated)
+#relay_log_info_repository=TABLE (deprecated)
+relay_log=pi1-relay-bin
+transaction_write_set_extraction=XXHASH64
+
+plugin_load_add='group_replication.so'
+group_replication_group_name="cd89fe36-a1ff-11eb-9ec1-0242ac110002"
+group_replication_start_on_boot=off
+group_replication_local_address="pi1.lan:33061"
+group_replication_group_seeds=""
+group_replication_bootstrap_group=off
+group_replication_recovery_public_key_path="public_key.pem"
+
+# InnoDB Cluster
+#
+binlog_transaction_dependency_tracking=WRITESET
+slave_parallel_type=LOGICAL_CLOCK
+slave_preserve_commit_order=ON
 EOF
 
 MYSQL_USER_ID=$(echo "${MYSQL_USER}" | xargs id -u)
